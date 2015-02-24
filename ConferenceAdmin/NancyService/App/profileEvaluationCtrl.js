ffee22467fb434227fd726d69b95497221bf2aa0
Randy <randy.soto@upr.edu>
2015-02-23 22:36:50 -0400

    Worked on many views

--- ConferenceAdmin/NancyService/App/profileEvaluationCtrl.js
+++ ConferenceAdmin/NancyService/App/profileEvaluationCtrl.js
@@ -10,6 +10,33 @@
     function profileEvaluationCtrl($scope, $http) {
         var vm = this;
 
+        vm.list = [{
+            name: "Randy Soto",
+            email: "randy.soto@upr.edu",
+            type: "Extended Paper",
+            accepted: true,
+            rejected: false
+        }, {
+            name: "Maria Rivera",
+            email: "maria.rivera30@upr.edu",
+            type: "Pannel",
+            accepted: false,
+            rejected: false
+        }, {
+            name: "Jaimeiris Nieves",
+            email: "jaimeiris.nieves@upr.edu",
+            type: "Poster",
+            accepted: false,
+            rejected: false
+        }, {
+            name: "Heidi Negron",
+            email: "heidi.negron1@upr.edu",
+            type: "Workshop",
+            accepted: false,
+            rejected: true
+        }];
+
+
         vm.activate = activate;
         vm.title = 'profileEvaluationCtrl';
 
@@ -20,3 +47,7 @@
 
     }
 })();
+
+
+
+
