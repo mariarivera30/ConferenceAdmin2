fecd139bf3c4a8a8413d78255cda3cfd44c2c41d
Heidi Negron-Arroyo <heidi.negron1@upr.edu>
2015-02-25 01:23:29 -0400

    registration, deadlines, venue

--- ConferenceAdmin/NancyService/App/participationInformation.js
+++ ConferenceAdmin/NancyService/App/participationInformation.js
@@ -13,44 +13,15 @@
         vm.activate = activate;
         vm.title = 'participationCtrl';
         vm.list = [{
-            name: "High School Student",
-            description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
-            cancelation: "Days of participation for conference to  Cancellation Policy bla bla ",
-            date1: "2/12/2015",
-            date2: "2/13/2015",
-            registrationPrice: "$25.00",
-            withoutFee: "$25.00",
-            withFee:"35.00"
-        }, {
-            name: "High School Student Companion",
-            description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
-            cancelation: "Days of participation for conference",
+            name: "University Student (Undergraduate & Graduate)",
+            description: "Poster Instructions",
             date1: "2/12/2015",
             date2: "2/13/2015",
             registrationPrice: "$25.00",
             withoutFee: "$25.00",
             withFee: "35.00"
-        }, {
-            name: "Graduate Student",
-            description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
-            date1: "2/12/2015",
-            date2: "2/13/2015",
-            registrationPrice: "$25.00",
-            withoutFee: "$25.00",
-            withFee: "35.00"
-        }, {
-            name: "Undegraduate Student",
-            cancelation: "Days of participation for conference",
-            description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
-            date1: "2/12/2015",
-            date2: "2/13/2015",
-            registrationPrice: "$25.00",
-            withoutFee: "$25.00",
-            withFee: "35.00"
-        },
-        {
+        },         {
             name: "Professional - Industry",
-            cancelation: "Days of participation for conference",
             description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
             date1: "2/12/2015",
             date2: "2/13/2015",
@@ -60,7 +31,23 @@
         },
         {
             name: "Professional - Academy",
-            cancelation: "Days of participation for conference",
+            description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
+            date1: "2/12/2015",
+            date2: "2/13/2015",
+            registrationPrice: "$25.00",
+            withoutFee: "$25.00",
+            withFee: "35.00"
+        },
+        {
+            name: "High School Student",
+            description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
+            date1: "2/12/2015",
+            date2: "2/13/2015",
+            registrationPrice: "$25.00",
+            withoutFee: "$25.00",
+            withFee:"35.00"
+        }, {
+            name: "High School Student- Companion",
             description: "Consider attending the first day of the conference where activities for female High School students will take place (hands on activities, presentations on computing, talk to females studying computing, attend the Puerto Rico Aspirations in Computing Award ceremony). Every high school student must be accompanied by a responsible adult (21 years or older).",
             date1: "2/12/2015",
             date2: "2/13/2015",
