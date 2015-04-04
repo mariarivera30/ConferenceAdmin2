9f94b4a8fb4f37fee007d1cc2b681b152bba9d93
mariarivera30 <maria.rivera30@upr.edu>
2015-04-04 05:49:22 -0400

    Redirection

--- ConferenceAdmin/NancyService/App/adminCtrl.js
+++ ConferenceAdmin/NancyService/App/adminCtrl.js
@@ -49,80 +49,84 @@
         }
 
         function _tabViewControl() {
+           // if(JSON.parse(sessionStorage.getItem('claims')) != )
             var list = JSON.parse(sessionStorage.getItem('claims'));
-       
-            list.forEach(function (claim) {
+            if (list != null) {
+                if (list.length > 0) {
+                    list.forEach(function (claim) {
 
-                if (claim.localeCompare('Master') == 0) {
-                    vm.administrator = true;
+                        if (claim.localeCompare('Master') == 0) {
+                            vm.administrator = true;
+                        }
+
+                        if (claim.localeCompare('Admin') == 0 || claim.localeCompare('Master') == 0) {
+                            //Website content tabs
+                            vm.participation = true;
+                            vm.general = true;
+                            vm.planning = true;
+                            vm.venue = true;
+                            vm.deadline = true;
+                            vm.registrationInt = true;
+                            vm.sponsorsInt = true;
+                            vm.contact = true;
+
+                            //Conference Manage tabs
+
+                            vm.committeeManage = true; //committe,admin,
+                            vm.topic = true; //committe,admin,
+                            vm.registrationList = true;
+                            vm.guest = true;
+                            vm.submissions = true;//committe,admin,
+                            vm.sponsors = true;
+                            vm.templates = true;//committe,admin,
+                            vm.authTemplates = true;
+                            vm.keyCodes = true;
+                            vm.reports = true;
+                            vm.isAdmin = true;
+                            vm.evaluators = true;
+
+                        }
+                        if (claim.localeCompare('Finance') == 0) {
+                            vm.general = true;
+                            vm.planning = true;
+                            vm.venue = true;
+                            vm.deadline = true;
+                            vm.registrationInt = true;
+                            vm.sponsorsInt = true;
+                            vm.contact = true;
+                            vm.participation = true;
+
+                            //Conference Manage tabs
+                            vm.committeeManage = false; //committe,admin,
+                            vm.topic = false; //committe,admin,
+                            vm.registrationList = true;
+                            vm.guest = true;
+                            vm.submissions = false;//committe,admin,
+                            vm.sponsors = true;
+                            vm.templates = false;//committe,admin,
+                            vm.authTemplates = false;
+                            vm.keyCodes = true;
+                            vm.reports = true;
+                            vm.isAdmin = true;
+                        }
+
+                        if (claim.localeCompare('CommitteEvaluator') == 0) {
+                            vm.submissions = true;//committe,admin,
+                            vm.templates = true;//committe,admin,
+                            vm.authTemplates = true;
+                            vm.topic = true; //committe,admin,
+                            vm.participation = false;
+                            vm.isAdmin = true;
+                            vm.evaluators = true;
+                        }
+
+
+
+
+                    });
                 }
-
-                if (claim.localeCompare('Admin') == 0 || claim.localeCompare('Master') == 0) {
-                    //Website content tabs
-                    vm.participation = true;
-                    vm.general = true;
-                    vm.planning = true;
-                    vm.venue = true;
-                    vm.deadline = true;
-                    vm.registrationInt = true;
-                    vm.sponsorsInt = true;
-                    vm.contact = true;
-
-                    //Conference Manage tabs
-
-                    vm.committeeManage = true; //committe,admin,
-                    vm.topic = true; //committe,admin,
-                    vm.registrationList = true;
-                    vm.guest = true;
-                    vm.submissions = true;//committe,admin,
-                    vm.sponsors = true;
-                    vm.templates = true;//committe,admin,
-                    vm.authTemplates = true;
-                    vm.keyCodes = true;
-                    vm.reports = true;
-                    vm.isAdmin = true;
-                    vm.evaluators = true;
-
-                }
-                if (claim.localeCompare('Finance') == 0) {
-                    vm.general = true;
-                    vm.planning = true;
-                    vm.venue = true;
-                    vm.deadline = true;
-                    vm.registrationInt = true;
-                    vm.sponsorsInt = true;
-                    vm.contact = true;
-                    vm.participation = true;
-
-                    //Conference Manage tabs
-                    vm.committeeManage = false; //committe,admin,
-                    vm.topic = false; //committe,admin,
-                    vm.registrationList = true;
-                    vm.guest = true;
-                    vm.submissions = false;//committe,admin,
-                    vm.sponsors = true;
-                    vm.templates = false;//committe,admin,
-                    vm.authTemplates = false;
-                    vm.keyCodes = true;
-                    vm.reports = true;
-                    vm.isAdmin = true;
-                }
-
-                if (claim.localeCompare('CommitteEvaluator') == 0) {
-                    vm.submissions = true;//committe,admin,
-                    vm.templates = true;//committe,admin,
-                    vm.authTemplates = true;
-                    vm.topic = true; //committe,admin,
-                    vm.participation = false;
-                    vm.isAdmin = true;
-                    vm.evaluators = true;
-                }
-
-
-               
-
-            });
-            if (!vm.isAdmin) {
+            }
+            else {
                 $location.path('/Home');
             }
 
