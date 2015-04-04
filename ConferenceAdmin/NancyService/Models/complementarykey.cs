0a6d538b864221beb6012b37323a6b2f7f2dc64d
mariarivera30 <maria.rivera30@upr.edu>
2015-04-04 03:17:56 -0400

    Se quedo una clase

--- ConferenceAdmin/NancyService/Models/complementarykey.cs
+++ ConferenceAdmin/NancyService/Models/complementarykey.cs
@@ -5,12 +5,17 @@ namespace NancyService.Models
 {
     public partial class complementarykey
     {
+        public complementarykey()
+        {
+            this.paymentcomplementaries = new List<paymentcomplementary>();
+        }
+
         public long complementarykeyID { get; set; }
         public long sponsorID { get; set; }
         public string key { get; set; }
         public Nullable<bool> isUsed { get; set; }
         public Nullable<bool> deleted { get; set; }
-        public virtual paymentcomplementary paymentcomplementary { get; set; }
+        public virtual ICollection<paymentcomplementary> paymentcomplementaries { get; set; }
         public virtual sponsor sponsor { get; set; }
     }
 }
