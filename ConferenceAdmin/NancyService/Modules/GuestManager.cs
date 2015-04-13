286617e70a41968499c59a0479cee669aacbe7c7
Jaimeiris <jaimeiris.nieves@gmail.com>
2015-04-13 09:34:37 -0400

    Altered Guestlist query

--- ConferenceAdmin/NancyService/Modules/GuestManager.cs
+++ ConferenceAdmin/NancyService/Modules/GuestManager.cs
@@ -16,7 +16,35 @@ namespace NancyService.Modules
             {
                 using (conferenceadminContext context = new conferenceadminContext())
                 {
-                    var guests = context.users.Where(c => c.hasApplied == true && c.deleted != true).Select(i => new GuestList
+                    List<GuestList> guestList = new List<GuestList>();
+                    List<user> guests = context.users.Where(c => c.hasApplied == true && c.deleted != true).ToList();
+                    long userID;
+                    String firstName;
+                    String lastName;
+                    String title;
+                    String affiliationName;
+                    String userTypeName;
+                    bool? authorizationStatus;
+                    bool isRegistered;
+                    String registrationStatus;
+                    String acceptanceStatus;
+                    String line1;
+                    String line2;
+                    String city;
+                    String state;
+                    String country;
+                    String zipcode;
+                    String email;
+                    String phoneNumber;
+                    String fax;
+                    bool? day1;
+                    bool? day2;
+                    bool? day3;
+                    String optionStatus;
+                    String companionFirstName;
+                    String companionLastName;
+                    long companionID;
+                    /*var guests = context.users.Where(c => c.hasApplied == true && c.deleted != true).Select(i => new GuestList
                     {
                         userID = (int)i.userID,
                         firstName = i.firstName,
@@ -44,9 +72,41 @@ namespace NancyService.Modules
                         companionFirstName = i.companions.FirstOrDefault() == null ? null : i.companions.FirstOrDefault().user.firstName,
                         companionLastName = i.companions.FirstOrDefault() == null ? null : i.companions.FirstOrDefault().user.lastName,
                         companionID = i.companions.FirstOrDefault() == null ? -1 : (long)i.companions.FirstOrDefault().userID 
-                    }).ToList();
-
-                    return guests;
+                    }).ToList();*/
+                    foreach (var guest in guests)
+                    {
+                        userID = guest.userID;
+                        firstName = guest.firstName;
+                        lastName = guest.lastName;
+                        title = guest.title;
+                        affiliationName = guest.affiliationName;
+                        userTypeName = guest.usertype.userTypeName;
+                        authorizationStatus = guest.minors.FirstOrDefault() == null ? false : guest.minors.FirstOrDefault().authorizationStatus;
+                        isRegistered = guest.registrationStatus == "Accepted" ? true : false;
+                        registrationStatus = guest.registrationStatus;
+                        acceptanceStatus = guest.acceptanceStatus;
+                        line1 = guest.address.line1;
+                        line2 = guest.address.line2;
+                        city = guest.address.city;
+                        state = guest.address.state;
+                        country = guest.address.country;
+                        zipcode = guest.address.zipcode;
+                        email = guest.membership.email;
+                        phoneNumber = guest.phone;
+                        fax = guest.userFax;
+                        day1 = guest.registrations.FirstOrDefault() == null ? null : guest.registrations.FirstOrDefault().date1;
+                        day2 = guest.registrations.FirstOrDefault() == null ? null : guest.registrations.FirstOrDefault().date2;
+                        day3 = guest.registrations.FirstOrDefault() == null ? null : guest.registrations.FirstOrDefault().date3;
+                        optionStatus = "Accepted";
+                        companionFirstName = guest.companions.FirstOrDefault() == null ? null : guest.companions.FirstOrDefault().user.firstName;
+                        companionLastName = guest.companions.FirstOrDefault() == null ? null : guest.companions.FirstOrDefault().user.lastName;
+                        companionID = guest.companions.FirstOrDefault() == null ? -1 : (long)guest.companions.FirstOrDefault().userID;
+                        guestList.Add(new GuestList(userID, firstName, lastName, title, affiliationName, userTypeName,
+              authorizationStatus, isRegistered, registrationStatus, acceptanceStatus, line1, line2,
+              city, state, country, zipcode, email, phoneNumber, fax, day1,
+              day2, day3, optionStatus, companionFirstName, companionLastName, companionID));
+                    }
+                    return guestList;
                 }
             }
             catch (Exception ex)
@@ -83,11 +143,12 @@ namespace NancyService.Modules
                 {
                     List<MinorAuthorizations> authorizations = new List<MinorAuthorizations>();
                     authorizations = context.authorizationsubmitteds.Where(c => c.minor.userID == id && c.deleted == false).
-                        Select(i => new MinorAuthorizations{
+                        Select(i => new MinorAuthorizations
+                        {
                             documentName = i.documentName,
                             documentFile = i.documentFile
-                        }).ToList();                    
-                   
+                        }).ToList();
+
                     return authorizations;
                 }
             }
@@ -126,17 +187,17 @@ namespace NancyService.Modules
     public class MinorAuthorizations
     {
         public String documentName;
-        public String documentFile;        
+        public String documentFile;
 
         public MinorAuthorizations()
         {
-            
+
         }
     }
 
     public class GuestList
     {
-        public int userID;
+        public long userID;
         public String firstName;
         public String lastName;
         public String title;
@@ -162,6 +223,40 @@ namespace NancyService.Modules
         public String companionFirstName;
         public String companionLastName;
         public long companionID;
+
+        public GuestList(long userID, String firstName, String lastName, String title, String affiliationName, String userTypeName,
+            bool? authorizationStatus, bool isRegistered, String registrationStatus, String acceptanceStatus, String line1, String line2,
+            String city, String state, String country, String zipcode, String email, String phoneNumber, String fax, bool? day1,
+            bool? day2, bool? day3, String optionStatus, String companionFirstName, String companionLastName, long companionID)
+        {
+            this.userID = userID;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.title = title;
+            this.affiliationName = affiliationName;
+            this.userTypeName = userTypeName;
+            this.authorizationStatus = authorizationStatus;
+            this.isRegistered = isRegistered;
+            this.registrationStatus = registrationStatus;
+            this.acceptanceStatus = acceptanceStatus;
+            this.line1 = line1;
+            this.line2 = line2;
+            this.city = city;
+            this.state = state;
+            this.country = country;
+            this.zipcode = zipcode;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+            this.fax = fax;
+            this.day1 = day1;
+            this.day2 = day2;
+            this.day3 = day3;
+            this.optionStatus = optionStatus;
+            this.companionFirstName = companionFirstName;
+            this.companionLastName = companionLastName;
+            this.companionID = companionID;
+
+        }
 
         public GuestList()
         {
