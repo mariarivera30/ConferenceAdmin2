7491ab718bec87c46fa2ae3f8fd8351246864fcb
Heidi Negron <heidi.negron1@upr.edu>
2015-04-16 17:19:38 -0400

    banner fix

--- ConferenceAdmin/NancyService/App/bannerCtrl.js
+++ ConferenceAdmin/NancyService/App/bannerCtrl.js
@@ -94,5 +94,22 @@
            .error(function (data, status, headers, config) {
            });
         }
+
+        //Carousel Play
+        $(function () {
+            $('#homeCarousel').carousel({
+                interval: 5000,
+                pause: "false"
+            });
+            $('#playButton').click(function () {
+                $('#homeCarousel').carousel('cycle');
+            });
+            $('#pauseButton').click(function () {
+                $('#homeCarousel').carousel('pause');
+            });
+        });
+
+
+
     }
 })();
