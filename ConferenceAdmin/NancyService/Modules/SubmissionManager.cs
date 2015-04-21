33f8822f2b84901d5b25e495401e21d440c4b5ca
Jaimeiris <jaimeiris.nieves@gmail.com>
2015-04-21 12:16:24 -0400

    Change document management in edition of submission to be more efficient as discussed in group meeting.

--- ConferenceAdmin/NancyService/Modules/SubmissionManager.cs
+++ ConferenceAdmin/NancyService/Modules/SubmissionManager.cs
@@ -1069,17 +1069,26 @@ namespace NancyService.Modules
 
                     if (submissionToEdit.submissionTypeID != 4)
                     {
-                        //delete every existent document bound to the submission
-                        List<documentssubmitted> documents = context.documentssubmitteds.Where(d => d.submissionID == sub.submissionID).ToList<documentssubmitted>();
-                        foreach (var doc in documents)
+                        //all documents in DB for submission with ID SubmissionID
+                        List<documentssubmitted> prevDocuments = context.documentssubmitteds.Where(d => d.submissionID == sub.submissionID).ToList<documentssubmitted>();
+                        //list of all new documents that are being added to the submission
+                        List<documentssubmitted> addedDocs = submissionToEdit.documentssubmitteds.Where(c => c.document != null ).ToList();
+                        //list of IDs of all documents that are in the DB and will not be removed from the submission
+                        List <long> remainingDocsID = submissionToEdit.documentssubmitteds.Where(c => c.document == null).Select(d => d.documentssubmittedID).ToList();
+                        //list of all documents that are in the DB and will not be removed from the submission
+                        List<documentssubmitted> remainingDocs = prevDocuments.Where(c => remainingDocsID.Contains(c.documentssubmittedID)).ToList();
+                        //list of all the documents that used to belong to the submission but where deleted by the user
+                        List<documentssubmitted> docsInDBtbd = prevDocuments.Except(remainingDocs).ToList();
+                        //remove from the DB all items delete by the user
+                        foreach (var docTBD in docsInDBtbd)
                         {
-                            context.documentssubmitteds.Remove(doc);
+                            context.documentssubmitteds.Remove(docTBD);
                         }
                         context.SaveChanges();
 
-                        //replace every document bound to the submission
+                        //add to the DB all new documents added to the submission
                         documentssubmitted subDocs = new documentssubmitted();
-                        foreach (var docs in submissionToEdit.documentssubmitteds)
+                        foreach (var docs in addedDocs)
                         {
                             subDocs.submissionID = sub.submissionID;
                             subDocs.documentName = docs.documentName;
@@ -1088,9 +1097,7 @@ namespace NancyService.Modules
                             context.documentssubmitteds.Add(subDocs);
                             context.SaveChanges();
                         }
-
                     }
-
                     return editedSub;
                 }
             }
